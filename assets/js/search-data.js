61162b1c4ba9c9c7f674c1e876eb772f81778f72
aidansharma98 <ahs181@scarletmail.rutgers.edu>
2026-03-15 21:16:28 +0000

    Deploying to gh-pages from @ aidansharma98/aidansharma98.github.io@077bf31d47a068bb457acf6b2cd30626936b769a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,14 +9,7 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-blog",
-          title: "Blog",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/blog/";
-          },
-        },{id: "nav-research",
+  },{id: "nav-research",
           title: "Research",
           description: "",
           section: "Navigation",
